a80720dbccb0646d79f8c8aaf3f31c6a9f443325
Marcin Kos <marcin.kos.krk@gmail.com>
2021-10-21 20:36:02 +0200

    branch fix

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -13,3 +13,10 @@ def test_homepage_status_code_by_reverse(client):
     response = client.get(url)
 
     assert response.status_code == 200
+
+
+def test_homepage_template(client):
+    url = reverse('home:home')
+    response = client.get(url)
+
+    assert 'home/home.html' in [t.name for t in response.templates]

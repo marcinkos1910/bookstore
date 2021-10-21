add5d8f444d5b459301c447195e23bf448ee8829
Marcin Kos <marcin.kos.krk@gmail.com>
2021-10-21 20:57:33 +0200

    all tests

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path('', include('home.urls')),
+    path('account/', include('django.contrib.auth.urls')),
 ]
--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -2,21 +2,35 @@ import pytest
 from django.urls import reverse
 
 
+@pytest.fixture
+def get_homepage_response(client):
+    url = reverse('home:home')
+    response = client.get(url)
+    return response
+
+
 def test_homepage_status_code(client):
     response = client.get('/')
 
     assert response.status_code == 200
 
 
-def test_homepage_status_code_by_reverse(client):
+def test_homepage_status_code_by_reverse(get_homepage_response):
     url = reverse('home:home')
-    response = client.get(url)
+    response = get_homepage_response.get(url)
 
-    assert response.status_code == 200
+    assert get_homepage_response.status_code == 200
 
 
-def test_homepage_template(client):
+def test_homepage_template(get_homepage_response):
     url = reverse('home:home')
-    response = client.get(url)
+    response = get_homepage_response.get(url)
 
-    assert 'home/home.html' in [t.name for t in response.templates]
+    assert 'home/home.html' in [t.name for t in get_homepage_response.templates]
+
+
+def test_homepage_contains_correct_html(get_homepage_response):
+    url = reverse('home:home')
+    response = get_homepage_response.get(url)
+
+    assert 'Welcome to bookstore' in get_homepage_response.content.decode('UTF-8')

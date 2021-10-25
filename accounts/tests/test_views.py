bbfe9cd467ed8e050e2bbef276d14db3729859fb
Marcin Kos <marcin.kos.krk@gmail.com>
2021-10-25 20:49:18 +0200

    tests for signup

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,31 @@
+import pytest
+from django.urls import reverse
+from django.test import Client
+from accounts.forms import CustomUserCreationForm
+
+
+@pytest.fixture
+def get_signup_response():
+    client = Client(enforce_csrf_checks=True)
+    url = reverse('accounts:signup')
+    response = client.get(url)
+    return response
+
+
+def test_signup_status_code(client):
+    response = client.get('/accounts/signup/')
+    assert response.status_code == 200
+
+
+def test_signup_reverse(get_signup_response):
+    assert get_signup_response.status_code == 200
+    assert 'registration/signup.html' in [t.name for t in get_signup_response.templates]
+    assert 'Sign Up' in get_signup_response.content.decode('UTF-8')
+
+
+def test_signup_form(get_signup_response):
+    form = get_signup_response.context.get('form')
+    assert isinstance(form, CustomUserCreationForm)
+    # assert 'csrfmiddlewaretoken' in get_signup_response
+
+

23da329c26ebe3dd94cfd6f44e76404bb75510af
Marcin Kos <marcin.kos.krk@gmail.com>
2021-10-27 00:09:56 +0200

    tests for about.html in views

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -34,3 +34,27 @@ def test_homepage_contains_correct_html(get_homepage_response):
     response = get_homepage_response.get(url)
 
     assert 'Welcome to bookstore' in get_homepage_response.content.decode('UTF-8')
+
+
+@pytest.fixture
+def get_about_page_response(client):
+    url = reverse('home:about')
+    response = client.get(url)
+    return response
+
+
+def test_about_page_status_code(client):
+    response = client.get('/about/')
+    assert response.status_code == 200
+
+
+def test_about_page_status_code_by_reverse(get_about_page_response):
+    assert get_about_page_response.status_code == 200
+
+
+def test_about_page_template(get_about_page_response):
+    assert 'home/about.html' in [t.name for t in get_about_page_response.templates]
+
+
+def test_about_page_contains_correct_html(get_about_page_response):
+    assert 'About' in get_about_page_response.content.decode('UTF-8')
